b7c26914dadc382c0f95f069792323abfbe30eed
andrewidiyanto <andretamvan@student.ub.ac.id>
2024-04-17 22:44:18 +0700

    update

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const client = new Client({
     authStrategy: new LocalAuth({
         dataPath: "sessions",
     }),
+    puppeteer: {
+        headless: true,
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--unhandled-rejections=strict"
+        ]
+    },
     webVersionCache: {
         type: 'remote',
         remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
